4e7b83d80dc9c59683a718a19ad35287acef0ff2
John Ryan <ryjohn@google.com>
2019-10-16 15:33:04 -0700

    remove unused import

--- lib/src/widgets/todo.dart
+++ lib/src/widgets/todo.dart
@@ -1,7 +1,6 @@
 import 'package:fb_todo/src/model/todo.dart';
 import 'package:flutter/material.dart';
 
-import 'package:fb_todo/src/services/services.dart';
 import 'package:flutter/services.dart';
 
 class TodoWidget extends StatefulWidget {
