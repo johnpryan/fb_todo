be6acff706eb5314b0fdc2969daa1538ad982d42
John Ryan <ryjohn@google.com>
2020-02-12 17:15:33 -0800

    use cloud_firestore for web version

---
+++ web/firebase_init.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyB3621RvSUbxgCPw3Dnq4uwKTjpZWILubI",
+    authDomain: "ryjohn-fb-todo.firebaseapp.com",
+    databaseURL: "https://ryjohn-fb-todo.firebaseio.com",
+    projectId: "ryjohn-fb-todo",
+    storageBucket: "ryjohn-fb-todo.appspot.com",
+    messagingSenderId: "942025734552",
+    // The name is also referred to as the app ID or project ID.
+    name: "1:942025734552:web:4ef259548602f97f08d3ed"
+};
+
+firebase.initializeApp(config);
